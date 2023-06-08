9efff409e0c77e296e20e7062427d917d0340818
INIRU <iniru@kakao.com>
2023-06-08 13:20:21 +0900

    Revert "Revert "human error fixed\nlogger debug view""
    This reverts commit afdb55886cf54cf695eb25d3a9eca9777f21ff7c.

--- src/utils.js
+++ src/utils.js
@@ -3,8 +3,8 @@ const logger = require('./logger.js');
 const getEnvironmentVariable = (value) => {
   const envValue = process.env[value];
   if (!envValue) {
-    logger.warn(`Env Variable is Null ${envValue}`);
-    throw new Error(`Env Variable is Null ${envValue}`);
+    logger.error(`Env Variable is Null ${value}`);
+    throw new Error(`Env Variable is Null ${value}`);
   } else return envValue;
 };
 
